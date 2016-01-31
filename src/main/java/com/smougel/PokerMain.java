be39e181d4cb6d8acbcd40ba906c54903c4c7f35
MrClockOrange <mougel.sylvain@gmail.com>
2016-01-31 20:57:39 +0100

    Multi table implementation

---
+++ src/main/java/com/smougel/PokerMain.java
@@ -0,0 +1,28 @@
+package com.smougel;
+
+import com.smougel.utils.Position;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by sylvainmougel on 11/01/16.
+ */
+public class PokerMain {
+
+
+    public static void main(String[] args) throws IOException, AWTException {
+        Properties tableProp = new java.util.Properties();
+        tableProp.load(ScreenScrapper.class.getResourceAsStream("/tables.properties"));
+        for (int i = 0; i < Integer.valueOf(tableProp.getProperty("windows.nb")); i++) {
+            Position windowPos = new Position(
+                    Integer.valueOf(tableProp.getProperty("windows." + i + ".ori.X")),
+                    Integer.valueOf(tableProp.getProperty("windows." + i + ".ori.Y"))
+            );
+            ScreenScrapper sc = new ScreenScrapper(windowPos, i);
+            System.out.println("Starting table " + i);
+            sc.start();
+        }
+    }
+}
